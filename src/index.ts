e5450111dce8e84890dd3ecace3566b5ff7d3549
Oleg-Lucenko <lucenko5577@gmail.com>
2023-12-26 23:34:16 +0200

    HW-4, Lucenko

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,72 @@
-interface Calculator {
-    x: number;
-    y: number;
+
+enum Actions {
+    '+' = 'add',
+    '-' = 'subtract',
+    '*' = 'multiply',
+    '/' = 'divide',
+
+};
+
+interface TheMathOperations {
     add(x:number, y:number):number;
     subtract(x:number, y:number):number;
     multiply(x:number, y:number):number;
     divide(x:number, y:number):number;
 };
 
-function calculate(data: Calculator): string {
-    let add = data.add(data.x, data.y);
-    let subtract = data.subtract(data.x, data.y);
-    let multiply = data.multiply(data.x, data.y);
-    let divide = data.divide(data.x, data.y);
-    return `Addition ${add}, subtraction ${subtract}, multiplication ${multiply}, division ${divide}`
+class Calculator implements TheMathOperations {
+    add(x:number, y: number): number {
+        return x + y;
+    };
+    subtract(x:number, y: number): number {
+        return x - y;
+    };
+    multiply(x:number, y: number): number {
+        return x * y;
+    };
+    divide(x:number, y: number): number {
+        if (y === 0) { throw new Error('You can`t divide by zero.')};
+        return x / y;
+    };
 };
+
+function calculate(calculator: Calculator, action: Actions, x: number, y: number): number {
+    return calculator[action](x, y);
+};
+
+let objForCalculation: Calculator = new Calculator();
+
+calculate(objForCalculation, Actions['/'], 36, 3);
+
+
+
 
 
 interface Book {
     bookName: string;
-    quantityPages: number;
+    authorName: string;
 };
 
 interface Author {
-    authorName: string;
-    surname: string;
+    bio: string;
 }
 
 interface BookService {
-    getBookName(): string;
-    getAuthorName(): string;
+    getBookName(book: Book): string;
+    getAuthorBio(author: Author): string;
 }
+
+class BookServiceImplementation implements BookService {
+
+    getBookName(book: Book): string {
+        return book.bookName;
+    };
+    getAuthorBio(author: Author): string {
+        return author.bio;
+    };
+};
+
+let bookService: BookServiceImplementation = new BookServiceImplementation();
 
 
 
